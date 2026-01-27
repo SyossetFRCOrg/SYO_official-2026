b6721c342184b46b99a2a30f2cac7a83ced282dc
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2026-01-27 18:41:10 -0500

    Removed reference to Angle measurement

--- src/main/java/frc/robot/subsystems/conveyor/ConveyorIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/conveyor/ConveyorIOTalonFX.java
@@ -14,7 +14,6 @@ import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
