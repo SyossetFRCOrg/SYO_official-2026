54a36d07b93c3253b36cbfc8e718b6de8a6a1f58
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2026-02-25 18:31:16 -0500

    Auto Chooser Enabled

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,7 +24,7 @@ public class Robot extends LoggedRobot {
 
   private final Field2d field = new Field2d();
 
-  // private AutoChooser autoChooser;
+  private AutoChooser autoChooser;
 
   private final RobotContainer robotContainer;
 
@@ -78,15 +78,14 @@ public class Robot extends LoggedRobot {
 
     SmartDashboard.putData(field);
 
-    // autoChooser =
-    // AutoChooser.create(
-    // robotContainer, robotContainer.getDrive(),
-    // robotContainer.getSuperstructure());
-    // Shuffleboard.getTab("Match")
-    // .add("Auto Program", autoChooser)
-    // .withSize(6, 3)
-    // .withPosition(12, 0)
-    // .withWidget(BuiltInWidgets.kComboBoxChooser);
+    autoChooser = AutoChooser.create(
+        robotContainer, robotContainer.getDrive(),
+        robotContainer.getSuperstructure());
+    Shuffleboard.getTab("Match")
+        .add("Auto Program", autoChooser)
+        .withSize(6, 3)
+        .withPosition(12, 0)
+        .withWidget(BuiltInWidgets.kComboBoxChooser);
   }
 
   @Override
