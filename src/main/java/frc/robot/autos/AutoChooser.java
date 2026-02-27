ed46b2e29f6e2ae20e1037cb04d43c48266282d1
niero369 <subscribe2niero@gmail.com>
2026-02-27 18:05:49 -0500

    added auton sequences for paths ending at the bump (for regal eagle)

--- src/main/java/frc/robot/autos/AutoChooser.java
+++ src/main/java/frc/robot/autos/AutoChooser.java
@@ -50,7 +50,20 @@ public class AutoChooser extends SendableChooser<Auto> {
     new AutoProgram(Auto.RSTART_OUTPOST_S2_TOWERRIGHT, "RSTART_OUTPOST_S2_TOWERRIGHT",
         autoFactory -> autoFactory.Outpost_S2_TowerRight(Location.RSTART)),
     new AutoProgram(Auto.MIDSTART_OUTPOST_S2_TOWERRIGHT, "MIDSTART_OUTPOST_S2_TOWERRIGHT",
-        autoFactory -> autoFactory.Outpost_S2_TowerRight(Location.MIDSTART))
+        autoFactory -> autoFactory.Outpost_S2_TowerRight(Location.MIDSTART)),
+
+    new AutoProgram(Auto.FLSTART_DEPOT_S3_LSTART, "FLSTART_DEPOT_S3_LSTART",
+        autoFactory -> autoFactory.Depot_S3_LStart(Location.FLSTART)),
+    new AutoProgram(Auto.LSTART_DEPOT_S3_LSTART, "LSTART_DEPOT_S3_LSTART",
+        autoFactory -> autoFactory.Depot_S3_LStart(Location.LSTART)),
+    new AutoProgram(Auto.MIDSTART_DEPOT_S3_LSTART,"FLSTART_DEPOT_S3_LSTART",
+        autoFactory -> autoFactory.Depot_S3_LStart(Location.MIDSTART)),
+    new AutoProgram(Auto.FRSTART_OUTPOST_S2_RSTART, "FRSTART_OUTPOST_S2_RSTART",
+        autoFactory -> autoFactory.Outpost_S2_RStart(Location.FRSTART)),
+    new AutoProgram(Auto.RSTART_OUTPOST_S2_RSTART, "RSTART_OUTPOST_S2_RSTART",
+        autoFactory -> autoFactory.Outpost_S2_RStart(Location.RSTART)),
+    new AutoProgram(Auto.MIDSTART_OUTPOST_S2_RSTART,"MIDSTART_OUTPOST_S2_RSTART",
+        autoFactory -> autoFactory.Outpost_S2_RStart(Location.MIDSTART))
   );
 
   /**
