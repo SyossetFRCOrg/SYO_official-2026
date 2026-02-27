d1dcac5de86a4dc1b6eecd9e8455d7c1ac139281
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2026-02-26 20:17:48 -0500

    Pigeon changes

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.MountPoseConfigs;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -24,7 +25,7 @@ public class GyroIOPigeon2 implements GyroIO {
   private final StatusSignal<AngularVelocity> yawVelocity = pigeon.getAngularVelocityZWorld();
 
   public GyroIOPigeon2() {
-    pigeon.getConfigurator().apply(new Pigeon2Configuration());
+    pigeon.getConfigurator().apply(new Pigeon2Configuration().withMountPose(new MountPoseConfigs().withMountPoseYaw(90)));
     pigeon.getConfigurator().setYaw(180.0);
     yaw.setUpdateFrequency(Drive.ODOMETRY_FREQUENCY);
     yawVelocity.setUpdateFrequency(100.0);
