0c1d053cfe46d2181fd5785b338b8a26974fae1f
Daniel Huang <huangdan0315@gmail.com>
2026-03-05 16:33:07 -0500

    created sotm file (tbd), changed kp to 12 for algin

---
+++ src/main/java/frc/robot/util/SOTMCalculator.java
@@ -0,0 +1,17 @@
+package frc.robot.util;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SOTMCalculator {
+  
+}
